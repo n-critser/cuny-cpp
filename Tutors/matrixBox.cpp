cc0b2955f22a6e5ff3791e66495c39b8b027e594
n-critser <critser101@gmail.com>
2014-03-21 00:12:07 -0400

    finished matrixBox

--- Tutors/matrixBox.cpp
+++ Tutors/matrixBox.cpp
@@ -1,11 +1,11 @@
 /*******************************************************************************
-  Title          : blank.cpp
+  Title          : matrixBox.cpp
   Author         : n-critser 
-  Created on     : 
-  Description    : 
-  Purpose        : 
+  Created on     : [2014-03-20]
+  Description    : A matrix is filled with no row, column conflicting values
+  Purpose        : A backtracking algorithm example
   Usage          : ./EXECUTABLE
-  Build with     : g++ -o blank  blank.cpp
+  Build with     : g++ -o matrixBox  matrixBox.cpp
   Modifications  : 
  
 *******************************************************************************/
@@ -14,6 +14,7 @@
 #include <cassert>
 #include <string>
 #include <time.h>
+#include <iomanip>
 time_t start,end;
 
 class mBox {
@@ -23,60 +24,143 @@ private:
         int ROWS;
         int COLS;
         int  matrix[9][9];
-
+        bool solve(int r,int  c);
+        int size;
 
 
 public:
-        mBox(int n): ROWS(n), COLS(n){}
+        mBox(int n): size(n),ROWS(n-1), COLS(n-1){}
         void fill();
-        void solve(int r,int  c);
+        void solve();
         bool test (int r, int c, int num);
         void display();
+        // Function to output class to ostream
+        friend std::ostream& operator<<
+	  ( std::ostream& os, const mBox& box );
 
 };
 
 
-void mBox::solve (int r, int c){
-        if (r==ROWS && c ==COLS){return;}
-        std::cout <<"entering the recursion"<<std::endl;
-        for (int i = 1; i < ROWS+1; i++)
-                if (test(r,c,i)  && matrix[r][c]){
-                        matrix[r][c]=i;
-                        if(c++ < COLS){
-                                std::cout<<"next col"<<std::endl;
-                                solve(r,c);
-                        }
-                        else{
-                                std::cout<<"next row"<< std::endl;
-                                solve(++r,0);
-                        }
-                }
-        
+std::ostream& operator<< ( std::ostream& os, const mBox& box ){
+        os << std::endl;
+        int size = box.size;
+
+        os << " +"; for(int i = 0; i < size*5-1; ++i) os << "-";
+        os << "+\n";
+
+        for(int i = 0; i < size; ++i){ 
+                os << " | "; 
+                for(int j = 0; j < size; ++j)
+                        os <<  std::setw(2) << box.matrix[i][j] << " | ";
+                os << std::endl;
+
+                os << " +"; for(int i = 0; i < size*5-1; ++i) os << "-";
+                os << "+\n";
+
+        }
+
+
+        return os;
+}
+
+void mBox::solve(){
+        solve(0,0);
+}
+
+
+void mBox::fill(){
+        for (int i = 0; i < size; i++)
+                for (int j= 0; j< size; j++)
+                        matrix[i][j]= -1;
 
 }
 
-        void mBox::display(){
-        for ( int i = 0 ; i < ROWS; i++){
+
+/*
+  Function: solve
+  -------------------------------------------------------------
+  usage: solve(0,0);
+  -------------------------------------------------------------
+  description:
+       Recursively submits numbers into the matrix and uses
+       backtracking.  If a value is set value conflicts it is
+       unset.  This process goes back and forth until all
+       values are set in the matrix or a solution is unable
+       to be found with the available values.
+  ------------------------------------------------------------
+ */
+bool mBox::solve (int r, int c){
+        //std::cout<<"solving["<<r<<"]"<<"["<<c<<"]"<<std::endl;
+        if (r==ROWS+1 && c ==0){
+                std::cout<< "solution found"<<std::endl;
+                return true;}
+        //std::cout <<"entering the recursion"<<std::endl;
+        for (int i = 1; i <= ROWS+1;i++ ){
+                if (test(r,c,i)  && matrix[r][c]==-1){
+                        matrix[r][c]=i;
+                        /*set [i][j] and try to solve the next element */
+                        int nextC=c;
+                        int nextR=r;
+                        
+                        nextC++;
+                        //std::cout << "nextR="<<nextR<<"\t nextC="<<nextC<<std::endl;
+                        if (nextC > COLS){
+                                nextC = 0;
+                                nextR++;
+                        }
+                        //std::cout<<"second solving["<<nextR<<"]"<<"["<<nextC<<"]"<<std::endl;
+                        if( solve(nextR,nextC)){
+                                //std::cout <<"solved all"<<std::endl;
+                                return true;
+                        }
+                }
+                matrix[r][c]=-1;
+                //std::cout<<"value not set["<<r<<"]["<<c<<"]"<<std::endl;
+              
+        }
+        
+        return false;
+}
+
+void mBox::display(){
+        for ( int i = 0 ; i < size; i++){
                 std::cout<<std::endl;
-                for( int j= 0; j< COLS; j++){
+                for( int j= 0; j< size; j++){
                         std::cout <<matrix[i][j];
                 }
         }
 }
-        bool mBox::test (int r, int c, int num){
-        /*for (int i = 0; i < r; i++){
-                if (matrix[i][c] == num){
-                        std::cout<<"match found in row ["<< i<<"]"<<std::endl;
-                        return false;
+
+/*
+  Function: test
+  -------------------------------------------------------------
+  usage: test (row,col,number);
+  -------------------------------------------------------------
+  description: returns the boolean value for any number of tests done on
+               the given number and all other values in the matrix.
+  
+ */
+bool mBox::test (int r, int c, int num){
+
+        
+        if( r > 0){
+                for (int i = 0; i < r; i++){
+                        if (matrix[i][c] == num){
+                                //std::cout<<"match found in row ["<< i<<"]"<<std::endl;
+                                return false;
+                        }
                 }
         }
+ 
+
         for (int j = 0; j< c ; j++ ){
                 if (matrix[r][j] == num){
-                        std::cout<< "match found in col ["<<j<<"]"<<std::endl;
+                        //std::cout<< "match found in at["<<r<<"]["<<j<<"]"<<std::endl;
                         return false;
                 }
         }
-        */
+        //std::cout<<std::endl;
+       
         return true;
 
 }
@@ -95,6 +179,12 @@ int main(int argc, char* argv[]){
 
         std::string addressFile="";
 
+
+        mBox matrix1(9);
+        matrix1.fill();
+        matrix1.solve();
+        //matrix1.display();
+        std::cout <<matrix1<<std::endl;
 	/*
 	 * If the program is ran without sufficient arguments it Will exit gracefully
 	 * with suggestions for the usage of the program.
@@ -105,17 +195,9 @@ int main(int argc, char* argv[]){
 	    return -1;
 	  }
         */
-         //int n = 5;
-         //int rows = n;
-         // int cols = n;
-         //int matrix[rows][cols];
-         
-        //solve(0,0);
-        // display();
-         
          time(&end);
          double dif = difftime (end,start);
-         std::cout << "elapsed time" << dif << std::endl;
+         std::cout << "\n elapsed time" << dif << std::endl;
          return 0;
 }
 
