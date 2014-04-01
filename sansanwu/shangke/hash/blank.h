4d672db7d1408b0abae0eef46528c76c05541811
n-critser <critser101@gmail.com>
2014-04-01 14:32:29 -0400

    stuff

---
+++ sansanwu/shangke/hash/blank.h
@@ -0,0 +1,37 @@
+/*******************************************************************************
+  Title          : blank.h
+  Author         : n-critser 
+  Created on     : 
+  Description    : 
+  Purpose        : 
+  Usage          : ./EXECUTABLE
+  Build with     : g++ -o 
+  Modifications  : 
+ 
+*******************************************************************************/
+
+
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+#include <string>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*Thats all*/
