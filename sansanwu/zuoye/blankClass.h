501b85564e026409862039278d970f44dec84528
n-critser <critser101@gmail.com>
2014-05-11 01:25:16 -0400

    cohort and heap

---
+++ sansanwu/zuoye/blankClass.h
@@ -0,0 +1,32 @@
+/*******************************************************************************
+  Title          : blank.h
+  Author         : n-critser 
+  Created on     : 
+  Description    : 
+  Purpose        : 
+  Usage          : ./EXECUTABLE
+  Build with     : g++ -o 
+  Modifications  : 
+ 
+*******************************************************************************/
+
+
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+#include <string>
+
+
+
+/* METHOD:
+   --------------------------------------------------------------------
+   USAGE: 
+   --------------------------------------------------------------------
+   DESCRIPTION: 
+
+  
+*/
+
+
+
+#endif /*Thats all*/
